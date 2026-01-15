d0cde468f73b3b79e4c2eb54d0beedc9d13f43a6
Andrea Damiano <decong.ad@gmail.com>
2026-01-15 17:20:06 +0100

    alien dictionary

--- algorithms/graph/alien_dictionary.py
+++ algorithms/graph/alien_dictionary.py
@@ -52,42 +52,100 @@ class Graph:
         node1.add_child(node2)
         self.edge_list.append(Edge(node1, node2))
 
+# def foreign_dictionary(words):
+#     result = ""
+#     graph = Graph()
+#     n = len(words)
+#     for i in range(1, n):
+#         #compare the current word with the previous 
+#         word_prev, word_curr = words[i-1] , words[i]
+#         l1 , l2 = len(word_prev), len(word_curr)
+#         min_len = min(l1, l2)
+
+#         if l1 > l2 and word_prev[:min_len] == word_curr[:min_len]:  # the 2 words are not lexicographically ordered
+#             return "" 
+        
+#         for j in range(min(l1, l2)):
+#             if word_prev[j] != word_curr[j]:
+#                 graph.add_edge(word_prev[j], word_curr[j])
+#                 break
+
+#     #visualize edges 
+#     for edge in graph.edge_list:
+#         print(edge)
+
+#     #create DAG
+#     heap = []
+#     for node in graph.nodes.values():
+#         node: Node
+#         if not node.parents:
+#             heapq.heappush(heap, node)
+
+#     while heap:
+#         current_node: Node
+#         current_node = heapq.heappop(heap)
+#         current_node.in_degree -= 1
+        
+#         for out_node in current_node.children:
+#             heapq.heappush(heap, out_node)
+        
+#         if current_node.in_degree > 0:
+#             heapq.heappush(heap, current_node)
+        
+#         else:
+#             result += f"{current_node.char}"
+            
+    
+#     return result
+
+
+
 def foreign_dictionary(words):
-    result = ""
-    graph = Graph()
+    adjency_list = {c: set() for w in words for c in w}
+    print(adjency_list)
+
     n = len(words)
     for i in range(1, n):
         #compare the current word with the previous 
         word_prev, word_curr = words[i-1] , words[i]
         l1 , l2 = len(word_prev), len(word_curr)
+        min_len = min(l1, l2)
+
+        if l1 > l2 and word_prev[:min_len] == word_curr[:min_len]:  # the 2 words are not lexicographically ordered
+            return "" 
+        
         for j in range(min(l1, l2)):
             if word_prev[j] != word_curr[j]:
-                graph.add_edge(word_prev[j], word_curr[j])
+                adjency_list[word_prev[j]].add(word_curr[j])
                 break
 
-    #create DAG
-    heap = []
-    for node in graph.nodes.values():
-        node: Node
-        if not node.parents:
-            heapq.heappush(heap, node)
+    adjency_list['o'] = set()
+    adjency_list['n'].add('o')
+    print(adjency_list)
 
-    while heap:
-        current_node: Node
-        current_node = heapq.heappop(heap)
-        current_node.in_degree -= 1
-        
-        for out_node in current_node.children:
-            heapq.heappush(heap, out_node)
-        
-        if current_node.in_degree > 0:
-            heapq.heappush(heap, current_node)
-        
-        else:
-            result += f"{current_node.char}"
-            
-    
-    return result
+    resopnse =  []
+    visited = {}
+
+    def dfs(char):
+        if char in visited:
+            return visited[char]  #if a node was already visited that means the letters are not lexicographically ordered 
+
+        visited[char] = True
+
+        for neighChar in adjency_list[char]:
+            if dfs(neighChar):
+                return True
+
+        #backtracking
+        visited[char] = False
+        resopnse.append(char)
+
+    for char in adjency_list:
+        if dfs(char):
+            return ""
+
+    resopnse.reverse()
+    return "".join(resopnse)
 
 if __name__ == "__main__":
     words = ["hrn","hrf","er","enn","rfnn"]
