57a3c06a30ef3555c6711f02eed8f19f78547acd
Andrea Damiano <decong.ad@gmail.com>
2026-02-08 22:59:40 +0100

    indentation

--- algorithms/Dynamic Programming/min_patches.py
+++ algorithms/Dynamic Programming/min_patches.py
@@ -2,19 +2,19 @@ from typing import List
 
 
 def minPatches(nums: List[int], n: int) -> int:
-        miss = 1
-        result = 0
-        i = 0
+    miss = 1   #unmatched number 
+    result = 0
+    i = 0
 
-        while miss <= n:
-            if i < len(nums) and nums[i] <= miss:
-                miss += nums[i]
-                i += 1
-            else:
-                miss += miss
-                result += 1
+    while miss <= n:
+        if i < len(nums) and nums[i] <= miss:
+            miss += nums[i]
+            i += 1
+        else:
+            miss += miss
+            result += 1
 
-        return result
+    return result
 
 
 if __name__ == "__main__":
