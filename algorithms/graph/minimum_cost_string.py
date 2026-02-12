1a56473144465c020342074522cbb41946eb7193
Andrea Damiano <decong.ad@gmail.com>
2026-02-12 23:22:28 +0100

    graph

--- algorithms/graph/minimum_cost_string.py
+++ algorithms/graph/minimum_cost_string.py
@@ -3,38 +3,42 @@ def minimumCost(source, target, original, changed, cost):
     INF = 10**30
     id = {}
     lens = set()
-    sz = 0
+    incremental_id = 0
 
     dist = [[INF]*201 for _ in range(201)]
 
+    #assign an id to every node (which is actually a string)
     for s, t, c in zip(original, changed, cost):
         if s not in id:
-            id[s] = sz
+            id[s] = incremental_id
             lens.add(len(s))
-            sz += 1
+            incremental_id += 1
         if t not in id:
-            id[t] = sz
-            sz += 1
+            id[t] = incremental_id
+            incremental_id += 1
         dist[id[s]][id[t]] = min(dist[id[s]][id[t]], c)
 
-    for i in range(sz):
+    #initialize the dp table of the floyd wharshall algorithm
+    for i in range(incremental_id):
         dist[i][i] = 0
 
-    for k in range(sz):
-        for i in range(sz):
+    #compute the shortest path of each node to each node 
+    for k in range(incremental_id):
+        for i in range(incremental_id):
             if dist[i][k] < INF:
-                for j in range(sz):
+                for j in range(incremental_id):
                     if dist[k][j] < INF:
                         dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
     n = len(source)
     dp = [INF] * (n + 1)
-    dp[0] = 0
+    dp[0] = 0  #represent the cost to convert source[:i] to target[:i]
 
     for i in range(n):
         if dp[i] == INF:
             continue
 
+        #if the 2 char are already identical no need to convert and to spend anything 
         if source[i] == target[i]:
             dp[i + 1] = min(dp[i + 1], dp[i])
 
