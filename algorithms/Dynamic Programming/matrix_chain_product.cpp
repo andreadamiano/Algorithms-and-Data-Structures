2c93e05c3ec4200b75ad9d44cd9c2a52fb125ee7
Andrea Damiano <decong.ad@gmail.com>
2026-06-22 00:02:00 +0200

    gdb

--- algorithms/Dynamic Programming/matrix_chain_product.cpp
+++ algorithms/Dynamic Programming/matrix_chain_product.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int matrixChainOrder(const vector<int>& p)
 {
     int n = p.size()-1; //number of matrix 
-    vector<vector <int> > dp (n+1, vector<int>(n+1, 0)); 
+    vector<vector <int> > dp (n+1, vector<int>(n+1, 0)); //dp[i][j] represent the smallest cost to multiply the matrices i:j
 
     for (int j=2; j< n+1; ++j)
     {
@@ -17,26 +17,14 @@ int matrixChainOrder(const vector<int>& p)
             //dp[i][j] is the cost of multiplying the matrix i times the matrix j 
             dp[i][j] = INT_MAX; //initialize upper triangular matrix to infinity
 
-            //try all possible splits between i and and j
+            //since matrix mulitplication is associative (but the cost is not) try all possible splits k between i and and j (divide and conquer approach) and memoize the min cost
             for (int k=i; k<j; ++k)
             {
-                //dp[i][k] + dp[k + 1][j] was the optimal cost to multiply i-k and k+1-j
+                //dp[i][k] + dp[k + 1][j] was the optimal cost to multiply i:k and k+1:j
                 //p[i - 1] * p[k] * p[j] is the cost of multiplying the resulting matrices 
                 int cost = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j]; //cost to multiply this 2 matrix 
                 if (cost < dp[i][j]) 
                     dp[i][j] = cost;
-
-
-                for (int i =1; i<n+1; ++i)
-                {
-                    for (int j=1; j<n+1; ++j)
-                    {   
-                        std::cout << dp[i][j] << " "; 
-                    }
-                    std::cout << "\n"; 
-                }
-                std::cout << "\n";
-
             }
 
         
@@ -71,8 +59,8 @@ int main ()
     // vector<int> p {30, 5, 10, 20, 40, 60, 40};
     vector<int> p {2, 10, 50, 20};
 
-    // std::cout << matrixChainOrder(p) << "\n";
+    std::cout << matrixChainOrder(p) << "\n";
     
-    std::cout << BruteForce(1, 3, p); 
+    // std::cout << BruteForce(1, 3, p); 
     
 }
