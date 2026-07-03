a68fea785573301167e79f78b633ad2ef288b0e3
Andrea Damiano <decong.ad@gmail.com>
2026-07-03 11:15:13 +0200

    TSP

--- algorithms/Dynamic Programming/TSP.py
+++ algorithms/Dynamic Programming/TSP.py
@@ -6,7 +6,7 @@ def notIn(node, subset):
 
 def combination(N, k):
     """
-    combination N choose k
+    combination N choose k without repetition
     """
     subsets = []
     combinations(0, 0, N, k, subsets)
@@ -21,56 +21,54 @@ def combinations(set, at, N, k, subsets: list):
         combinations(set, i+1, N, k-1, subsets)
         set ^= (1 << i) #backtracking
 
-def setup(graph, memo, S, N):
+def setup(graph, dp, S, N):
     """
-    Initialize the memo table storing the optimal solution from the start node to every other node
-    The dp table store: memo[visited_set][last_node]
+    Initialize the dp table storing the optimal solution from the start node to every other node
+    The dp table store: dp[visited_set][last_node]
     since the cost to reach a set of nodes depends both on the order and on the visited nodes
     the base case is the cost of reaching the nodes directly accessible from S
     """
     for i in range(N):
 
         #asign to the subset of the starting node and the next node the weight of the edge connecting them 
-        memo[(1 << S) | (1 << i)][i] = graph[S][i]
+        dp[(1 << S) | (1 << i)][i] = graph[S][i]
 
-def solve(graph, memo, S, N):
+def solve(graph, dp, S, N):
     #this first loop represent the number of nodes in a partial tour
     #loop over all partial tours starting from a tour of 3 nodes (since 2 nodes have been already initialized)
-    for r in range(3, N+1):
+    for tour_len in range(3, N+1):
         #N choose r combination without repetition
         #this generate all possible partial tours of lenght r starting from node S
-        # for subset in combination(range(N), r):
-        #generate all possible tour of 3 nodes 
-        for subset in combination(N, r):
+        for tour_set in combination(N, tour_len):
             #S must be part of the generated tour 
-            if notIn(S, subset):
+            if notIn(S, tour_set):
                 continue  
 
             #loop over all possible nodes to find a possible next node,
             #which is found if it's not the starting node, but if it's part of the current subset
             #so for each subset find a possible next node
-            for next in range(N):
-                if next == S or notIn(next, subset):
+            for nextNode in range(N):
+                if nextNode == S or notIn(nextNode, tour_set):
                     continue
                 #this is the subset without the next node,
-                #to look up in the memo table the optimal cost to reach next
+                #to look up in the dp table the optimal cost to reach next
                 #take the tour without the next node 
-                state = subset ^ (1 << next)
+                prev_tour_set = tour_set ^ (1 << nextNode)
 
                 minDistance = float("inf")
                 #endnode represent the last node before reaching the next node
                 #so in this algorithm we "brute force" over all possible previous node with all possibel next node
-                for endNode in range(N):
-                    if endNode == S  or notIn(endNode, state) :
+                for prevNode in range(N):
+                    if prevNode == S  or notIn(prevNode, prev_tour_set):
                         continue
 
-                    minDistance = min(memo[state][endNode] + graph[endNode][next], minDistance)
+                    minDistance = min(dp[prev_tour_set][prevNode] + graph[prevNode][nextNode], minDistance)
 
-                #save the result in the memo table
-                memo[subset][next] = minDistance
+                #save the result in the dp table
+                dp[tour_set][nextNode] = minDistance
 
 
-def findMinCost(graph, memo, S, N):
+def findMinCost(graph, dp, S, N):
     #bitmask with all bits sets to 1 , which represent the final state
     #containing the optimum cost to visit each nodes until the semi last node,
     #before going back to the last
@@ -83,13 +81,13 @@ def findMinCost(graph, memo, S, N):
             continue
 
         #compute the cost to go back to the starting node
-        tourCost = memo[end_state][endNode]  + graph[endNode][S]
+        tourCost = dp[end_state][endNode]  + graph[endNode][S]
         if tourCost < minTourCost:
             minTourCost = tourCost
 
     return minTourCost
 
-def findOptimalTour(graph, memo, S, N):
+def findOptimalTour(graph, dp, S, N):
     #start backtracking from the starting node
     lastIndex = S
     state = (1 << N) -1  #end state where all nodes have been visited
@@ -104,7 +102,7 @@ def findOptimalTour(graph, memo, S, N):
         for j in range(N):
             if j == S or notIn(j, state):
                 continue
-            newDist = memo[state][j] + graph[j][lastIndex]
+            newDist = dp[state][j] + graph[j][lastIndex]
             if newDist < minDist:
                 minDist = newDist
                 minNode = j
@@ -121,14 +119,14 @@ def findOptimalTour(graph, memo, S, N):
 def TSP(graph, start):
     N = len(graph)
 
-    memo = [[float('inf')] * N for _ in range(1 << N)] #2d DP table of size [2^N][N]
+    dp = [[float('inf')] * N for _ in range(1 << N)] #2d DP table of size [2^N][N]
 
-    setup(graph, memo, S, N)
-    solve(graph, memo, S, N)
-    print(f"Memo table: {memo}")
-    minCost = findMinCost(graph, memo, S, N)
+    setup(graph, dp, S, N)
+    solve(graph, dp, S, N)
+    print(f"dp table: {dp}")
+    minCost = findMinCost(graph, dp, S, N)
     print(f"MinCosst: {minCost}")
-    tour = findOptimalTour(graph, memo, S, N)
+    tour = findOptimalTour(graph, dp, S, N)
     print(f"Tour: {tour}")
 
     return (minCost, tour)
