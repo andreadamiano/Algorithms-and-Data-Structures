da8c070e23e03dcde94da5522992297bac4982af
Andrea Damiano <decong.ad@gmail.com>
2026-08-05 22:56:28 +0200

    imporved comments

--- algorithms/Dynamic Programming/longest_increasing_subsequence.py
+++ algorithms/Dynamic Programming/longest_increasing_subsequence.py
@@ -1,13 +1,30 @@
-# def lis(array):
-#     n = len(array)
-#     dp = [1 for _ in range(n)]
+import bisect
 
-#     for i in range (1, n):
-#         for j in range(i):
-#             if (array[i] > array[j]):
-#                 dp[i] = max(dp[i], dp[j] +1)  # check if the ith element can extend the subsequence ending at j 
+def lis(array):
+    n = len(array)
+    dp = [1 for _ in range(n)]
 
-#     return dp[-1]
+    for i in range (1, n):
+        for j in range(i):
+            if (array[i] > array[j]):
+                dp[i] = max(dp[i], dp[j] +1)  # check if the ith element can extend the subsequence ending at j 
+
+    return dp[-1]
+
+def lis(array):
+    """binary search lis"""
+    dp = []
+    for num in array:
+        index = bisect.bisect_left(dp, num)  
+        if index == len(dp):
+            dp.append(num)   # we can safely extend the lis 
+
+        else:
+            dp[index] = num  #replace to give the possibility to extend 
+        print(dp)
+
+    # return len(dp)
+    return lis_array(dp, array)   #dp is maintaining the smallest possible end elements for increasing subsequences of various lengths
 
 
 def lis_array(dp, array):
@@ -24,23 +41,6 @@ def lis_array(dp, array):
         
 
     return dp
-
-
-import bisect
-def lis(array):
-    """binary search lis"""
-    dp = []
-    for num in array:
-        index = bisect.bisect_left(dp, num)  
-        if index == len(dp):
-            dp.append(num)   # we can safely extend the lis 
-
-        else:
-            dp[index] = num  #replace to give the possibility to extend 
-        print(dp)
-
-    # return len(dp)
-    return lis_array(dp, array)   #dp is maintaining the smallest possible end elements for increasing subsequences of various lengths
 
 if __name__ == "__main__":
     array = [10, 22, 9, 33, 21, 50, 41, 60, 80]
