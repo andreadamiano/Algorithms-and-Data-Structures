fdb58d4157d90373b31e6c1dd03c74ff82410f6f
Andrea Damiano <decong.ad@gmail.com>
2026-08-25 23:59:14 +0200

    refined logic

--- algorithms/Greedy/jump_game.py
+++ algorithms/Greedy/jump_game.py
@@ -2,7 +2,7 @@ def jump(nums):
     n = len(nums)
     end = farthest = n_jumps = 0
     i = 0
-    while end +1 < n:
+    while end < n-1:
         n_jumps += 1 
         while i <= end and i <= n:
             farthest = max(farthest, i + nums[i])
@@ -17,8 +17,8 @@ def jump(nums):
 
 
 if __name__ == "__main__":
-    # nums = [2,3,1,1,4]
+    nums = [2,3,1,1,4]
     # nums = [2,3,0,1,4]
     # nums = [0]
-    nums = [3,4,3,2,5,4,3]
+    # nums = [3,4,3,2,5,4,3]
     print(jump(nums))
